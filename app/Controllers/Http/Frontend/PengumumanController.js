f1a4d678aaad4d2d38c95c23c5559ec69a90285f
chimey <selamet.antsoftmedia@g.gmail.com>
2024-07-05 04:42:29 +0700

    fix

--- app/Controllers/Http/Frontend/PengumumanController.js
+++ app/Controllers/Http/Frontend/PengumumanController.js
@@ -274,195 +274,36 @@ class PengumumanController {
     );
     var current_time = dateFormat(date.getTime(), "HH:MM:ss");
 
-    if (current_date == tanggal_pengumuman) {
-      if (
-        this.timeStringToFloat(current_time) >=
-        this.timeStringToFloat(profilsekolah.jam_pengumuman)
-      ) {
-        const rules = {
-          nisn: "required|min:10|max:10|number",
-        };
+    try {
+      const peserta = await Pesarta.query().where("nisn", nisn).first();
 
-        const messages = {
-          "nisn.required": "NISN harus diisi",
-          "nisn.min": "NISN tidak benar (10 Digit)",
-          "nisn.max": "NISN tidak benar (10 Digit)",
-          "nisn.number": "NINS tidak benar (ANGKA)",
-        };
+      const data = {};
 
-        const Validation = await validate(request.all(), rules, messages);
+      data["nisn"] = peserta.nisn;
+      data["nopend"] = peserta.nopend;
+      data["nama"] = peserta.nama;
+      data["ttl"] =
+        peserta.tempat_lahir +
+        ", " +
+        dateFormat(peserta.tanggal_lahir, "dd/mm/yyyy");
+      data["jalur"] = peserta.jalur;
+      data["jarak"] = peserta.jalur == "Zonasi" ? peserta.jarak : "";
+      data["show_jarak"] = peserta.jalur == "Zonasi" ? true : false;
+      data["verifikasi"] = peserta.verifikasi;
+      data["status_lulus"] = peserta.status == "L" ? true : false;
+      data["asal_sekolah"] = peserta.asal_sekolah;
+      data["kelas"] = peserta.kelas;
+      data["show"] = true;
 
-        if (Validation.fails()) {
-          const msg = await Validation.messages();
-          return response.json({
-            status: false,
-            message: msg[0].message,
-            data: {
-              nisn: null,
-              nopend: null,
-              nama: null,
-              jalur: null,
-              show_jarak: false,
-              jarak: null,
-              verifikasi: null,
-              status_lulus: null,
-              asal_sekolah: null,
-              kelas: null,
-              ttl: null,
-              show: false,
-            },
-          });
-        }
-
-        try {
-          const peserta = await Pesarta.query().where("nisn", nisn).first();
-
-          const data = {};
-
-          data["nisn"] = peserta.nisn;
-          data["nopend"] = peserta.nopend;
-          data["nama"] = peserta.nama;
-          data["ttl"] =
-            peserta.tempat_lahir +
-            ", " +
-            dateFormat(peserta.tanggal_lahir, "dd/mm/yyyy");
-          data["jalur"] = peserta.jalur;
-          data["jarak"] = peserta.jalur == "Zonasi" ? peserta.jarak : "";
-          data["show_jarak"] = peserta.jalur == "Zonasi" ? true : false;
-          data["verifikasi"] = peserta.verifikasi;
-          data["status_lulus"] = peserta.status == "L" ? true : false;
-          data["asal_sekolah"] = peserta.asal_sekolah;
-          data["kelas"] = peserta.kelas;
-          data["show"] = true;
-
-          return response.json({
-            status: true,
-            message: "Sukses",
-            data: data,
-          });
-        } catch (error) {
-          return response.json({
-            status: false,
-            message: "Opps..., terjadi kesalahan Data tidak ditemukan",
-            data: {
-              nisn: null,
-              nopend: null,
-              nama: null,
-              ttl: null,
-              jalur: null,
-              show_jarak: false,
-              jarak: null,
-              verifikasi: null,
-              status_lulus: null,
-              asal_sekolah: null,
-              kelas: null,
-            },
-          });
-        }
-      } else {
-        return response.json({
-          status: false,
-          message: "Opps...,maaf jadwal pengumuman belum di buka",
-          data: {
-            nisn: null,
-            nopend: null,
-            nama: null,
-            ttl: null,
-            jalur: null,
-            show_jarak: false,
-            jarak: null,
-            verifikasi: null,
-            status_lulus: null,
-            asal_sekolah: null,
-            kelas: null,
-          },
-        });
-      }
-    } else if (new Date(current_date) >= new Date(tanggal_pengumuman)) {
-      const rules = {
-        nisn: "required|min:10|max:10|number",
-      };
-
-      const messages = {
-        "nisn.required": "NISN harus diisi",
-        "nisn.min": "NISN tidak benar (10 Digit)",
-        "nisn.max": "NISN tidak benar (10 Digit)",
-        "nisn.number": "NINS tidak benar (ANGKA)",
-      };
-
-      const Validation = await validate(request.all(), rules, messages);
-
-      if (Validation.fails()) {
-        const msg = await Validation.messages();
-        return response.json({
-          status: false,
-          message: msg[0].message,
-          data: {
-            nisn: null,
-            nopend: null,
-            nama: null,
-            ttl: null,
-            jalur: null,
-            show_jarak: false,
-            jarak: null,
-            verifikasi: null,
-            status_lulus: null,
-            asal_sekolah: null,
-            kelas: null,
-            show: false,
-          },
-        });
-      }
-
-      try {
-        const peserta = await Pesarta.query().where("nisn", nisn).first();
-
-        const data = {};
-
-        data["nisn"] = peserta.nisn;
-        data["nopend"] = peserta.nopend;
-        data["nama"] = peserta.nama;
-        data["ttl"] =
-          peserta.tempat_lahir +
-          ", " +
-          dateFormat(peserta.tanggal_lahir, "dd/mm/yyyy");
-        data["jalur"] = peserta.jalur;
-        data["jarak"] = peserta.jalur == "Zonasi" ? peserta.jarak : "";
-        data["show_jarak"] = peserta.jalur == "Zonasi" ? true : false;
-        data["verifikasi"] = peserta.verifikasi;
-        data["status_lulus"] = peserta.status == "L" ? true : false;
-        data["asal_sekolah"] = peserta.asal_sekolah;
-        data["kelas"] = peserta.kelas;
-        data["show"] = true;
-
-        return response.json({
-          status: true,
-          message: "Sukses",
-          data: data,
-        });
-      } catch (error) {
-        return response.json({
-          status: false,
-          message: "Opps..., terjadi kesalahan Data tidak ditemukan",
-          data: {
-            nisn: null,
-            nopend: null,
-            nama: null,
-            ttl: null,
-            jalur: null,
-            show_jarak: false,
-            jarak: null,
-            verifikasi: null,
-            status_lulus: null,
-            asal_sekolah: null,
-            kelas: null,
-          },
-        });
-      }
-    } else {
+      return response.json({
+        status: true,
+        message: "Sukses",
+        data: data,
+      });
+    } catch (error) {
       return response.json({
         status: false,
-        message: "Opps...,maaf jadwal pengumuman belum di buka",
+        message: "Opps..., terjadi kesalahan Data tidak ditemukan",
         data: {
           nisn: null,
           nopend: null,
@@ -478,6 +319,7 @@ class PengumumanController {
         },
       });
     }
+
   }
 
   timeStringToFloat(time) {
